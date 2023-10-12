8e7250975cabbec301a088392d4a86a32619513b
snavy <1337snavy@gmail.com>
2023-10-12 01:47:20 +0000

    6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ python """
+import typing
 
 
-def sum_mixed_list(input_list: list[float | int]) -> float:
+def sum_mixed_list(input_list: typing.List[typing.Union[float, int]]) -> float:
     """ sum_mixed_list """
     result = 0.0
     for v in input_list:
