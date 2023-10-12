7ce68dda78980c5055b24f4fb2f0625eb991f716
snavy <1337snavy@gmail.com>
2023-10-12 01:52:46 +0000

    9

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" python """
+import typing
+
+
+def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+    """ element_length """
+    return [(i, len(i)) for i in lst]
