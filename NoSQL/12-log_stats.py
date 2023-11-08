76cd371c5760222d760aa856ee40d05da5dc83f6
snavy <1337snavy@gmail.com>
2023-11-08 13:19:31 +0100

    12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,22 +7,28 @@ def main():
     """ main """
     client = pymongo.MongoClient("mongodb://localhost:27017")
     collection = client.logs.nginx
-    print('%s logs' % (collection.count_documents()))
+    print('%s logs' % (collection.count_documents({})))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
         find_len_display(collection, method)
+    status = [
+        v
+        for v
+        in collection.find({'method': 'GET', 'path': '/status'})
+    ]
+    print('%s status check' % (len(status)))
 
 
 def find(collection, method):
     """ find """
     regex = method
-    return collection.find_many({'method': {'$regex': regex}})
+    return collection.find({'method': {'$regex': regex}})
 
 
 def find_len(collection, method):
     """ find len """
-    return len(find(collection, method))
+    return len([v for v in find(collection, method)])
 
 
 def find_len_display(collection, method):
