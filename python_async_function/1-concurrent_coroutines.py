237c1f681c16cf84056dd992a667025bb524742a
snavy <1337snavy@gmail.com>
2023-11-08 11:04:32 +0100

    1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import asyncio
+import typing
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 """ python """
 
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     result = [await result for result in asyncio.as_completed([asyncio.create_task(wait_random(max_delay)) for _ in range(n)])]
