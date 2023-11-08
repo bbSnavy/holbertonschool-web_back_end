2f1cfeefd50bfc3334976a6bbfbbf91ec0991cab
snavy <1337snavy@gmail.com>
2023-11-08 11:47:41 +0100

    1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,4 +7,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> typing.List[float]:
     """ async comprehension """
-    return [float(v) async for v in async_generator()]
+    result = [v async for v in async_generator()]
+    result = [float(v) for v in result]
+    return result
