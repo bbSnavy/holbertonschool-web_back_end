c599869f5e90167fe190a13c5243578fc9cd9daf
snavy <1337snavy@gmail.com>
2023-11-08 12:44:31 +0100

    11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,4 +4,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """ schools by topic """
-    return mongo_collection.find({"topic": topic})
+    return mongo_collection.find({"topics": topic})
