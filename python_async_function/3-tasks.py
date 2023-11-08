7a039be87bc138973c356182707f0c17bfb04307
snavy <1337snavy@gmail.com>
2023-11-08 11:20:25 +0100

    3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -2,7 +2,7 @@
 """ tasks """
 import asyncio
 
-wait_random = __import__('0-basic_async_syntex').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
