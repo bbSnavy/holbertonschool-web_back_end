1ae55a7a92e18ac4b5dd46479133063930693aa7
snavy <1337snavy@gmail.com>
2023-11-08 11:37:09 +0100

    0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ async generator """
 import asyncio
 import random
+import typing
 
 
-async def async_generator():
+async def async_generator() -> typing.Generator[float, None, None]:
     """ async generator """
     for _ in range(10):
         await asyncio.sleep(1)
