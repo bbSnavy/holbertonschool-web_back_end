90b1ec83f632ad5fd0d5fea6f121dba45faa8127
snavy <1337snavy@gmail.com>
2023-11-08 11:09:12 +0100

    1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
+""" wait_n """
 import asyncio
 import typing
 
 wait_random = __import__('0-basic_async_syntax').wait_random
-
-""" python """
 
 
 async def wait_n(n: int, max_delay: int) -> typing.List[float]:
