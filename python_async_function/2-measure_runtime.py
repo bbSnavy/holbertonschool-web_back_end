7827e182efe952d5a9fc3afc4c502b5bb5725076
snavy <1337snavy@gmail.com>
2023-11-08 11:15:00 +0100

    2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" measure_runtime """
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines.py')
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """ measure_time """
+    s = time.time()
+
+    await wait_n(n, max_delay)
+
+    e = time.time()
+
+    return e - s
