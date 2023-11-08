057dedbf2e4f91e58a6bf01f9791732d1d5df2cb
snavy <1337snavy@gmail.com>
2023-11-08 12:55:22 +0100

    12

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" log stats """
+import pymongo
+
+
+def main():
+    """ main """
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    collection = client.logs.nginx
+    print('%s logs' % (len(collection.find_many())))
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        find_len_display(method)
+
+
+def find(collection, method):
+    """ find """
+    return collection.find_many({'method': method})
+
+
+def find_len(collection, method):
+    return len(find(collection, method))
+
+
+def find_len_display(collection, method)):
+    print('\tmethod %s: %s' % (method, find_len(collection, method)))
+
+
+if __name__ == '__main__':
+    main()
