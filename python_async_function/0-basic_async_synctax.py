541b0f58de4d3c50c37843ce27796456d37b817e
snavy <1337snavy@gmail.com>
2023-11-08 10:54:23 +0100

    0

---
+++ python_async_function/0-basic_async_synctax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import random
+""" python """
+
+
+async def wait_random(max_delay: int = 10):
+    """ wait_random """
+    v = random.uniform(0.0, float(max_delay))
+
+    await asyncio.sleep(v)
+
+    return v
