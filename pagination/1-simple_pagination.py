894c09bb04bb3f5e7dc6d6e0f15e32321d6ce00e
snavy <1337snavy@gmail.com>
2023-11-08 15:35:45 +0100

    1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -30,6 +30,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> typing.List[typing.List]:
+        assert type(page) is int
+        assert type(page_size) is int
         assert page > 0
         assert page_size > 0
 
