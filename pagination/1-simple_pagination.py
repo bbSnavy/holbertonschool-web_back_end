f143b55a2ec605176d4f7f7a0e4649e15146182c
snavy <1337snavy@gmail.com>
2023-11-08 15:34:53 +0100

    1

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" simple pagination """
+import csv
+import math
+import typing
+
+
+def index_range(page: int, page_size: int) -> typing.Tuple[int]:
+    """ index range """
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> typing.List[typing.List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> typing.List[typing.List]:
+        assert page > 0
+        assert page_size > 0
+
+        data = self.dataset()
+        page = index_page(page, page_size)
+        if page[1] >= len(data):
+            return []
+
+        return data[page[0]: page[1]]
