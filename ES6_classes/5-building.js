84bbe80b06b5f3ca57fbe7590bf02c7c4064e384
snavy <1337snavy@gmail.com>
2023-11-12 21:33:09 +0000

    5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,25 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw TypeError('sqft must be a number');
+    }
+
+    this._sqft = sqft;
+
+    if (this.constructor !== Building && this.evacuationWarningMessage == undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  set sqft(value) {
+    if (typeof sqft !== 'number') {
+      throw TypeError('sqft must be a number');
+    }
+
+    this._sqft = value;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
