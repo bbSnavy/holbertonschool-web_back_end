49b2efe2c4e6f134746e858cdaaa86aba2e750a9
snavy <1337snavy@gmail.com>
2023-11-12 18:03:32 +0000

    5

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,17 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-  let result = [];
-
-  for (let i = 0; i < array1.length; i++) {
-    result.push(array1[i]);
-  }
-
-  for (let i = 0; i < array2.length; i++) {
-    result.push(array2[i]);
-  }
-
-  for (let i = 0; i < string.length; i++) {
-    result.push(string[i]);
-  }
-
-  return result;
+  return [...array1, ...array2, ...string];
 }
